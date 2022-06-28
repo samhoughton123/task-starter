204f37ee4a54dbcdc658d539344fff3f86f43ac6
Sam Houghton <sam@houghton.io>
2022-06-28 18:15:01 +0100

    Removes old framework

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,8 @@
 import '../styles/main.scss'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-
-  useEffect(() => {
-    AOS.init();
-  }, [])
 
   return (
     <Layout>
